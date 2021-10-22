b0617740042b244839bf992e946caaae5ad81963
Woo-jinJeon <jeenie37@gmail.com>
2021-10-22 14:52:26 +0900

    gitignore

--- app.py
+++ app.py
@@ -1,13 +1,3 @@
 from streamlit_webrtc import webrtc_streamer
-import av
 
-class VideoProcessor:
-    def recv(self, frame):
-        img = frame.to_ndarray(format="bgr24")
-
-        flipped = img[::-1,:,:]
-
-        return av.VideoFrame.from_ndarray(flipped, format="bgr24")
-
-
-webrtc_streamer(key="example", video_processor_factory=VideoProcessor)
+webrtc_streamer(key="example")
