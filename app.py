d4251b0b3c749ff90c994089d8c2c178c0ad5e94
Woo-jinJeon <jeenie37@gmail.com>
2021-11-09 22:47:44 +0900

    sp squats

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from streamlit_webrtc import webrtc_streamer
+from streamlit_webrtc import webrtc_streamer, WebRtcMode
+
 import av
 from utils.ShoulderP import ShoulderP
 from utils import my_helpers
@@ -9,13 +10,14 @@ import streamlit as st
 
 class VideoProcessor:
     def __init__(self):
-        self.framework = 'tflite'
-        self.model_variant = 'rt_lite'
-        # self.framework = 'tensorflow'
-        # self.model_variant = 'ii'
+        # self.framework = 'tflite'
+        # self.model_variant = 'rt_lite'
+        self.framework = 'tensorflow'
+        self.model_variant = 'ii'
         self.model, self.resolution = my_helpers.get_model(self.framework, self.model_variant)
 
         self.goal = 0
+        self.mode = ""
 
     def recv(self, frame):
         start = time.time()
@@ -23,21 +25,23 @@ class VideoProcessor:
         frame, frame_coordinates, frame_height,frame_width = \
             my_helpers.std_process(frame,self.model_variant, self.model, self.resolution,self.framework)
 
-        frame = ShoulderP.run_shoulderp(frame, frame_coordinates, frame_height, frame_width)
-
-        print(self.goal)
+        if self.mode == 'Shoulder Press':
+            frame = ShoulderP.run_shoulderp(frame, frame_coordinates, frame_height, frame_width)
+        elif self.mode == "Squats":
+            pass
         # print('takes', time.time()-start)
         return av.VideoFrame.from_ndarray(frame, format="bgr24")
 
 def run():
-    print('this also runs')
     st.title('조상님이 보고있다')
-    st.selectbox("", ['Shoulder Press', "Squats"])
+    mode = st.selectbox("", ['Shoulder Press', "Squats"])
     ctx = webrtc_streamer(key="example", video_processor_factory=VideoProcessor,
-                    media_stream_constraints={"video": {"frameRate": {"ideal": 20}}})
-    goal = st.select_slider('how many', [i for i in range(1, 21)])
+                    media_stream_constraints={"video": {"frameRate": {"ideal": 20}}},mode=WebRtcMode.SENDRECV)
+    goal = st.select_slider('How many?', [i for i in range(1, 21)])
     if ctx.video_processor:
         ctx.video_processor.goal = goal
+        ctx.video_processor.mode = mode
+
 
 
 run()
