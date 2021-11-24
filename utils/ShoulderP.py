15860ffb7f000ed8e8d10b2488ba43429ac1298d
Woo-jinJeon <jeenie37@gmail.com>
2021-11-25 04:46:38 +0900

    thisseemsfine

--- utils/ShoulderP.py
+++ utils/ShoulderP.py
@@ -118,38 +118,38 @@ class ShoulderP(Workouts):
         elif wrist_y >= threshold:
             return False
 
-    # @staticmethod
-    # def image_alpha(frame, body_part_x, body_part_y, marker_radius, RGB, alpha, frame_count, fold_frame, fill = True):
-    #     '''
-    #
-    #         Args:
-    #             body_part_x: coordinate_x (The center of circle)
-    #             body_part_y: coordinate_y (The center of circle
-    #             marker_radius: circle radius
-    #             RGB: Tuple RGB color
-    #             alpha: transparent intensity of circle
-    #
-    #         Returns: Image with circle
-    #
-    #         '''
-    #
-    #     image_ = cv2.circle(frame, (int(body_part_x), int(body_part_y)), marker_radius, (255, 255, 255), 2)
-    #     if fill:
-    #         mask_rad = int(frame_count * marker_radius / (fold_frame))
-    #         mask1 = np.ones_like(frame) * 255  # 흰 배경 검은 원
-    #         mask1 = cv2.circle(mask1, (int(body_part_x), int(body_part_y)), mask_rad, (0, 0, 0), -1)
-    #         mask2 = cv2.bitwise_not(mask1)  # 검은 배경 힌색 원
-    #         mask_image = cv2.bitwise_and(image_, mask1)  # 원 외부의 이미지 가져오기
-    #         blend1 = cv2.bitwise_and(image_, mask2)  # 원 내부의 이미지 가져오기
-    #         blend2 = cv2.circle(mask2, (int(body_part_x), int(body_part_y)), mask_rad, RGB, -1)
-    #         blended = mask_image + blend1 * (1 - alpha) + blend2 * alpha
-    #         blended = blended.astype(np.uint8)
-    #         return blended
-    #     else:
-    #         return image_
-    #     #
-    #     # blended = cv2.resize(cv2.flip(blended, 1), (1000, 1000))
-    #     # return cv2.imshow('EfficientPose (Groos et al., 2020)', blended)
+    @staticmethod
+    def image_alpha(frame, body_part_x, body_part_y, marker_radius, RGB, alpha, frame_count, fold_frame, fill = True):
+        '''
+
+            Args:
+                body_part_x: coordinate_x (The center of circle)
+                body_part_y: coordinate_y (The center of circle
+                marker_radius: circle radius
+                RGB: Tuple RGB color
+                alpha: transparent intensity of circle
+
+            Returns: Image with circle
+
+            '''
+
+        image_ = cv2.circle(frame, (int(body_part_x), int(body_part_y)), marker_radius, (255, 255, 255), 2)
+        if fill:
+            mask_rad = int(frame_count * marker_radius / (fold_frame))
+            mask1 = np.ones_like(frame) * 255  # 흰 배경 검은 원
+            mask1 = cv2.circle(mask1, (int(body_part_x), int(body_part_y)), mask_rad, (0, 0, 0), -1)
+            mask2 = cv2.bitwise_not(mask1)  # 검은 배경 힌색 원
+            mask_image = cv2.bitwise_and(image_, mask1)  # 원 외부의 이미지 가져오기
+            blend1 = cv2.bitwise_and(image_, mask2)  # 원 내부의 이미지 가져오기
+            blend2 = cv2.circle(mask2, (int(body_part_x), int(body_part_y)), mask_rad, RGB, -1)
+            blended = mask_image + blend1 * (1 - alpha) + blend2 * alpha
+            blended = blended.astype(np.uint8)
+            return blended
+        else:
+            return image_
+        #
+        # blended = cv2.resize(cv2.flip(blended, 1), (1000, 1000))
+        # return cv2.imshow('EfficientPose (Groos et al., 2020)', blended)
 
     @staticmethod
     def validity(landmark):
